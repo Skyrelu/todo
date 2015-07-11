174ad067f1628a917df2882fb782aeb95530d345
Skyrelu <731285362@qq.com>
2015-07-11 11:19:56 +0800

    创建页面元素

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#sidebar-icon-new").on('click',function(){
+  	$("#first-line").toggleClass('icon-line-rotated');
+  	$("#second-line").toggleClass('icon-line-rotated');
+  });	
+});
